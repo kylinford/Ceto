4c550006cdfc31dd8af16d6a241f516207ae8f9e
Jiaqi Wang <jackyjqw@gmail.com>
2022-06-15 16:05:09 +0800

    resume to high quality

---
+++ Assets/Ceto/Scripts/Utility/TextFPS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ceto;
+using UnityEngine.UI;
+
+namespace Ceto
+{
+    public class TextFPS : MonoBehaviour
+    {
+        public Common.Unity.Utility.FPSCounter m_fps;
+        public Text text;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            text.text = "Current FPS = " + m_fps.FrameRate.ToString("F2");
+        }
+    }
+}
+
